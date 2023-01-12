dba21a7c79101acb9763c5799a48114f954dc532
Steve Crouch <s.crouch@software.ac.uk>
2023-01-12 12:49:26 +0000

    Convert to pytest, add requirements file

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,15 +1,13 @@
-import unittest
+import pytest
 from mymath.factorial import factorial
 
 
-class TestFactorialFunctions(unittest.TestCase):
+def test_3():
+    assert factorial(3) == 6
 
-  def test_3(self):
-    self.assertEqual(factorial(3), 6)
+def test_5():
+    assert factorial(5) == 120
 
-  def test_5(self):
-    self.assertEqual(factorial(5), 120)
-
-  def test_negative(self):
-    with self.assertRaises(ValueError):
+def test_negative():
+    with pytest.raises(ValueError):
       factorial(-1)
